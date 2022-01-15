0076f478a42347ffdeaa3f99ab766dc7d9e15808
mushixin <15809265710@qq.com>
2022-01-15 20:53:42 +0800

    update

--- src/chapter_3_wanhao/Problem_10_RecoverBST_redo.java
+++ src/chapter_3_wanhao/Problem_10_RecoverBST_redo.java
@@ -11,6 +11,39 @@ import utils.InputUtils.*;
 
 public class Problem_10_RecoverBST_redo {
 
+    /**
+     * 中序遍历，找到nodes列表中，前面的结点。
+     * @param head
+     * @param nodes
+     * @return
+     */
+    public static Node findBefNodes(Node head,Node nodes){
+        Node bef = null;
+        Set<Node> marked = new HashSet<>();
+        LinkedList<Node> temp = new LinkedList<>();
+        temp.addFirst(head);
+        while (!temp.isEmpty()) {
+            Node node = temp.removeFirst();
+            if (marked.contains(node)) {
+                if (node==nodes){
+                    System.out.println(nodes.value+"前一个结点是"+ bef.value);
+                    return bef;
+                }
+                bef = node;
+                continue;
+            }
+            marked.add(node);
+            if (node.right != null) {
+                temp.addFirst(node.right);
+            }
+            temp.addFirst(node);
+            if (node.left != null) {
+                temp.addFirst(node.left);
+            }
+        }
+        return null;
+    }
+
 
     public static Node[] getTwoErrNodes(Node head) {
         Node[] errs = new Node[2];
@@ -18,6 +51,9 @@ public class Problem_10_RecoverBST_redo {
         if (head == null) {
             return errs;
         }
+        /**
+         * 前一个结点，当前异常结点，后一个节点
+         */
         Set<Node> marked = new HashSet<>();
         LinkedList<Node> temp = new LinkedList<>();
         temp.addFirst(head);
@@ -27,6 +63,8 @@ public class Problem_10_RecoverBST_redo {
             if (marked.contains(node)) {
                 if (tempLastNum>node.value){
                     errs[errNum++] = node;
+                    tempLastNum = node.value;
+                    continue;
                 }
                 tempLastNum = node.value;
                 continue;
@@ -44,147 +82,21 @@ public class Problem_10_RecoverBST_redo {
         return errs;
     }
 
-
-    public static Node[] getTwoErrParents(Node head, Node e1, Node e2) {
-        Node[] parents = new Node[2];
-        if (head == null) {
-            return parents;
-        }
-        Stack<Node> stack = new Stack<Node>();
-        while (!stack.isEmpty() || head != null) {
-            if (head != null) {
-                stack.push(head);
-                head = head.left;
-            } else {
-                head = stack.pop();
-                if (head.left == e1 || head.right == e1) {
-                    parents[0] = head;
-                }
-                if (head.left == e2 || head.right == e2) {
-                    parents[1] = head;
-                }
-                head = head.right;
-            }
-        }
-        return parents;
-    }
-
     public static Node recoverTree(Node head) {
         Node[] errs = getTwoErrNodes(head);
-        Node[] parents = getTwoErrParents(head, errs[0], errs[1]);
-        Node e1 = errs[0];
-        Node e1P = parents[0];
-        Node e1L = e1.left;
-        Node e1R = e1.right;
-        Node e2 = errs[1];
-        Node e2P = parents[1];
-        Node e2L = e2.left;
-        Node e2R = e2.right;
-        if (e1 == head) {
-            if (e1 == e2P) { // ���һ
-                e1.left = e2L;
-                e1.right = e2R;
-                e2.right = e1;
-                e2.left = e1L;
-            } else if (e2P.left == e2) { // �����
-                e2P.left = e1;
-                e2.left = e1L;
-                e2.right = e1R;
-                e1.left = e2L;
-                e1.right = e2R;
-            } else { // �����
-                e2P.right = e1;
-                e2.left = e1L;
-                e2.right = e1R;
-                e1.left = e2L;
-                e1.right = e2R;
-            }
-            head = e2;
-        } else if (e2 == head) {
-            if (e2 == e1P) { // �����
-                e2.left = e1L;
-                e2.right = e1R;
-                e1.left = e2;
-                e1.right = e2R;
-            } else if (e1P.left == e1) { // �����
-                e1P.left = e2;
-                e1.left = e2L;
-                e1.right = e2R;
-                e2.left = e1L;
-                e2.right = e1R;
-            } else { // �����
-                e1P.right = e2;
-                e1.left = e2L;
-                e1.right = e2R;
-                e2.left = e1L;
-                e2.right = e1R;
-            }
-            head = e1;
-        } else {
-            if (e1 == e2P) {
-                if (e1P.left == e1) { // �����
-                    e1P.left = e2;
-                    e1.left = e2L;
-                    e1.right = e2R;
-                    e2.left = e1L;
-                    e2.right = e1;
-                } else { // �����
-                    e1P.right = e2;
-                    e1.left = e2L;
-                    e1.right = e2R;
-                    e2.left = e1L;
-                    e2.right = e1;
-                }
-            } else if (e2 == e1P) {
-                if (e2P.left == e2) { // �����
-                    e2P.left = e1;
-                    e2.left = e1L;
-                    e2.right = e1R;
-                    e1.left = e2;
-                    e1.right = e2R;
-                } else { // ���ʮ
-                    e2P.right = e1;
-                    e2.left = e1L;
-                    e2.right = e1R;
-                    e1.left = e2;
-                    e1.right = e2R;
-                }
-            } else {
-                if (e1P.left == e1) {
-                    if (e2P.left == e2) { // ���ʮһ
-                        e1.left = e2L;
-                        e1.right = e2R;
-                        e2.left = e1L;
-                        e2.right = e1R;
-                        e1P.left = e2;
-                        e2P.left = e1;
-                    } else { // ���ʮ��
-                        e1.left = e2L;
-                        e1.right = e2R;
-                        e2.left = e1L;
-                        e2.right = e1R;
-                        e1P.left = e2;
-                        e2P.right = e1;
-                    }
-                } else {
-                    if (e2P.left == e2) { // ���ʮ��
-                        e1.left = e2L;
-                        e1.right = e2R;
-                        e2.left = e1L;
-                        e2.right = e1R;
-                        e1P.right = e2;
-                        e2P.left = e1;
-                    } else { // ���ʮ��
-                        e1.left = e2L;
-                        e1.right = e2R;
-                        e2.left = e1L;
-                        e2.right = e1R;
-                        e1P.right = e2;
-                        e2P.right = e1;
-                    }
-                }
-            }
+        Node beferrs = findBefNodes(head, errs[0]);
+
+        //若是errs中有一个结点
+        if (errs[1]==null){
+            int temp = errs[0].value;
+            errs[0].value = beferrs.value;
+            beferrs.value = temp;
+            return head;
         }
+        //若是有两个结点
+        int temp = beferrs.value;
+        beferrs.value = errs[1].value;
+        errs[1].value=temp;
         return head;
     }
 
