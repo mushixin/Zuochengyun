a0d8b4bcbcd195498ce687db69c3c4a1ca1a1da0
mushixin <15809265710@qq.com>
2022-01-29 12:36:59 +0800

    update

--- src/interview/Solution.java
+++ src/interview/Solution.java
@@ -2,37 +2,68 @@ package interview;
 
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
 
-    /**
-     * 编码后的最小长度。
-     *
-     * @param words
-     * @return
-     */
-    public static int minimumLengthEncoding(String[] words) {
-        /**
-         * 长的先生成，若是某个串x是y的子串，应该把y先拼接。
-         */
-        Arrays.sort(words, (o1, o2) -> o2.length() - o1.length());
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < words.length; ++i) {
-            if (sb.indexOf(words[i] + "#") == -1) {
-                sb.append(words[i]);
-                sb.append("#");
-            }
+        int[] cards;
+
+        //将两个数字加减乘除，结果放到set中
+        public Set<Double> operate(double a,double b){
+            Set<Double> set  = new HashSet<>();
+            set.add(a+b);
+            set.add(a-b);
+            set.add(b-a);
+            set.add(a*b);
+            set.add(a/(double)b);
+            set.add(b/(double)a);
+            return set;
         }
-        return sb.length();
-    }
+
+        public boolean judgePoint24(int[] cards) {
+            this.cards = cards;
+            Arrays.sort(cards);
+            Set<Double>set12 = operate(cards[0],cards[1]);
+            Set<Double>set13 = operate(cards[0],cards[2]);
+            Set<Double>set14 = operate(cards[0],cards[3]);
+            Set<Double>set23 = operate(cards[1],cards[2]);
+            Set<Double>set24 = operate(cards[1],cards[3]);
+            Set<Double>set34 = operate(cards[2],cards[3]);
+
+            for(Double d1:set12){
+                for(Double d2:set34){
+                    if(operate(d1,d2).contains(24)){
+                        return true;
+                    }
+                }
+            }
+            for(Double d1:set13){
+                for(Double d2:set24){
+                    if(operate(d1,d2).contains(24)){
+                        return true;
+                    }
+                }
+            }
+            for(Double d1:set14){
+                for(Double d2:set23){
+                    if(operate(d1,d2).contains(24)){
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+
+        }
+
 
     public static void main(String[] args) {
-        String[] words = {"time", "me", "bell"};
-        System.out.println(minimumLengthEncoding(words));
+        System.out.println(new interview.Solution().judgePoint24(new int[]{4,1,8,7}));
 
-        String[] words2 = {"time", "ime2", "bell"};
-        System.out.println(minimumLengthEncoding(words2) == "tim2#ime2#bell#".length());//tim2#ime2#bell#
+
+
     }
 }
--- src/leecode/Solution.java
+++ src/leecode/Solution.java
@@ -1,57 +1,72 @@
 package leecode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public int leastBricks(List<List<Integer>> wall) {
-        ArrayList<Long>gapCount = new ArrayList<>();
-        for(List<Integer>wallTemp:wall){
-            long column=0;
-            for(int i=0;i<wallTemp.size()-1;++i){
-                column = column + wallTemp.get(i);
-                gapCount.add(column);
+    Set<String> dead = new HashSet<>();
+    //从最终字符串，走到key这一步，最少需要走几步
+    Map<String,Integer> map = new HashMap<>();
+
+
+    public int openLock(String[] deadends, String target) {
+        for(String str:deadends){
+            dead.add(str);
+        }
+        
+        map.put(target,0);
+        LinkedList<String> temp = new LinkedList<>();
+        LinkedList<String>tempNext = new LinkedList<>();
+
+        temp.add(target);
+        int currentStep = 0;
+        while(!temp.isEmpty()){
+            String first = temp.removeFirst();
+            if(first.equals("0000")){
+                return currentStep;
+            }
+            addNextStep(first,tempNext,currentStep+1);
+
+            if(temp.isEmpty()){
+                temp = tempNext;
+                tempNext = new LinkedList<>();
+                currentStep++;
             }
         }
-        gapCount.sort(new Comparator<Long>(){
-            public int compare(Long a,Long b){
-                return (int)(a-b);
+        return -1;
+    }
+
+    //若是这个str的下一个操作，没有操作过，则操作，并加入到链表中。并且存入map
+    public void addNextStep(String str, LinkedList<String>tempNext, int step){
+        StringBuilder sb = new StringBuilder(str);
+        for(int pos=0;pos<=3;++pos){
+            String s1 = addMinus(sb,pos,-1);
+            String s2 = addMinus(sb,pos,1);
+            if(!map.containsKey(s1) && !dead.contains(s1)){
+                map.put(s1,step);
+                tempNext.addLast(s1);
             }
-        });
-        //统计每列的缝隙，选择缝隙最多的那一列。count[i]表示第i个缝隙，[1,sum-1]个缝隙。
-        int maxGap = 0;
-        for(int i=0;i<gapCount.size();++i){
-            int start = i;
-            int end = start + maxGap;
-            while(end<gapCount.size() && gapCount.get(end).longValue()==gapCount.get(start).longValue()){
-                i = end;
-                end++;
-            }
-            maxGap = Math.max(maxGap,end-start);
-            if(maxGap>=wall.size()){
-                break;
+            if(!map.containsKey(s2)  && !dead.contains(s1)){
+                map.put(s2,step);
+                tempNext.addLast(s2);
             }
         }
-        return wall.size()-maxGap;
+    }
+
+    //pos=0,1,2,3   val=1/-1
+    public String addMinus(StringBuilder str,int pos,int val){
+        String bef = str.substring(0,pos);
+        char c= (char)(str.charAt(pos)+val);
+        if(c<'0'){
+            c+=10;
+        }else if(c>'9'){
+            c-=10;
+        }
+        String temp = str.substring(pos+1);
+        return  bef+c+temp;
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> wall = new ArrayList<>();
-        List<Integer>w1 = new ArrayList<>();
-        w1.add(10000_0000);
-        w1.add(10000_0000);
-        w1.add(100);
-        w1.add(100);
-        System.out.println(w1.get(0)==w1.get(1));
-
-
-//        List<Integer>w2 = new ArrayList<>();
-//        w2.add(10000_0000);
-//        w2.add(10000_0000);
-//
-//        wall.add(w1);
-//        wall.add(w2);
-//        System.out.println(new Solution().leastBricks(wall));
+        String[]dead = {"0201","0101","0102","1212","2002"};
+        System.out.println(new Solution().openLock(dead,"0202"));
     }
 }
