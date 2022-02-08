ab97bd92d33a8554f211eda32af1801416e90e32
mushixin <15809265710@qq.com>
2022-02-08 20:53:01 +0800

    update

--- src/chapter_7_bitoperation/Problem_01_SwapWithoutTmp.java
+++ src/chapter_7_bitoperation/Problem_01_SwapWithoutTmp.java
@@ -2,16 +2,73 @@ package chapter_7_bitoperation;
 
 public class Problem_01_SwapWithoutTmp {
 
-	public static void main(String[] args) {
-		int a = 16;
-		int b = 111;
-		System.out.println(a);
-		System.out.println(b);
-		a = a ^ b;
-		b = a ^ b;
-		a = a ^ b;
-		System.out.println(a);
-		System.out.println(b);
-	}
+    public static int add(int a, int b) {
+        int yu = (a & b);
+        int hu = (a ^ b);
+        while (yu != 0) {
+            yu = (yu << 1);
+            int yu2 = (yu & hu);
+            int hu2 = (yu ^ hu);
+            yu = yu2;
+            hu = hu2;
+        }
+        return hu;
+    }
+
+    public static String int2String(int n) {
+        StringBuilder sb = new StringBuilder();
+
+//		for (int i = 0; i < 32; i++) {
+//			if((n & (1 << i))==0){
+//				sb.insert(0, '0');
+//			}else{
+//				sb.insert(0, '1');
+//			}
+//		}
+
+		int model = 1;
+        while (model !=(1<<31)) {
+            if ((n & model) == 0) {
+                sb.insert(0, '0');
+            } else {
+                sb.insert(0, '1');
+            }
+			model = (model<<1);
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+//		int a = 16;
+//		int b = 111;
+//		System.out.println(a);
+//		System.out.println(b);
+//		a = a ^ b;
+//		b = a ^ b;
+//		a = a ^ b;
+//		System.out.println(a);
+//		System.out.println(b);
+
+//		System.out.println(add(1,1));
+//		System.out.println(add(1,2));
+//		System.out.println(add(2,2));
+
+//        int2String(1);
+//		int2String(-2);
+//		int2String(-1); // 取反码+1  0000000000000001
+
+//		System.out.println(add(-1,2));
+
+
+        //交换两个变量的值
+        int a = 1;//01
+        int b = 2;//10
+        a = a^b;//11
+        b = a^b;//01
+        a = a^b;//
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
 
 }
--- src/leecode/Solution.java
+++ src/leecode/Solution.java
@@ -1,75 +1,79 @@
-package leecode;/*
-// Definition for a Node.
-*/
+package leecode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
-    public static class Node {
-        public int val;
-        public List<Node> neighbors;
-        public Node() {
-            val = 0;
-            neighbors = new ArrayList<Node>();
+    int[]matchsticks;
+    int length;//每条边的长度
+    int currentLen = 0;//当前总长度
+    boolean[]unUsed;// 没有使用火柴的index
+
+    public boolean makesquare(int[] matchsticks) {
+        this.matchsticks = matchsticks;
+        if(matchsticks.length<=3){
+            return false;
         }
-        public Node(int _val) {
-            val = _val;
-            neighbors = new ArrayList<Node>();
+        length = 0;
+        for(int i=0;i<matchsticks.length;++i){
+            length+=matchsticks[i];
         }
-        public Node(int _val, ArrayList<Node> _neighbors) {
-            val = _val;
-            neighbors = _neighbors;
+        if((length%4)!=0){
+            return false;
         }
+        Arrays.sort(matchsticks);
+        length = length/4;
+        if(matchsticks[matchsticks.length-1]>length){
+            return false;
+        }
+        unUsed = new boolean[matchsticks.length];
+
+        //先把最长的放入
+        for(int i=0;i<matchsticks.length-1;++i){
+            unUsed[i] = true;
+        }
+        currentLen = matchsticks[matchsticks.length-1];
+        return dfs();
     }
 
-
-    Map<Integer,Node> setNode = new HashMap<>();
-    boolean[][]map;
-
-
-    public void bsf(Node node){
-        if(node==null || setNode.containsKey(node.val)){
-            return ;
+    //用到了第index个火柴了
+    public boolean dfs(){
+        //拼好了三条边就完成了
+        if(length*3 == currentLen){
+            return true;
         }
-        setNode.put(node.val, new Node(node.val));
-        for(Node temp:node.neighbors){
-            bsf(temp);
-        }
-    }
-    public void mapMark(Node node){
-        if(node==null || map[node.val][node.val]){
-            return ;
-        }
-        map[node.val][node.val] = true;
-        for(Node temp:node.neighbors){
-            map[node.val][temp.val] = true;
-            mapMark(temp);
-        }
-    }
-
-    public Node buildTree(Node node){
-        if(!map[node.val][node.val]){
-            return node;
-        }
-        map[node.val][node.val] = false;
-
-        //将这个结点的关系填充下
-        for(int i=1;i<map.length;++i){
-            if(map[node.val][i] && node.val!=i){
-                node.neighbors.add(buildTree(setNode.get(i)));
+        for(int index=matchsticks.length-2;index>=0;--index){
+            if(unUsed[index] && canput(matchsticks[index])){
+                currentLen += matchsticks[index];
+                unUsed[index] = false;
+                boolean res = dfs();
+                if(res){
+                    return true;
+                }
+                currentLen -= matchsticks[index];
+                unUsed[index] = true;
             }
+
         }
-        return node;
+        return false;
     }
 
-    public Node cloneGraph(Node node) {
-        bsf(node);//记录所有结点。
-        map= new boolean[setNode.size()+1][setNode.size()+1];
-        System.out.println(setNode);        
-        return   buildTree(setNode.get(node.val));
+    public boolean canput(int stickLen){
+        //之前 必须小于length
+        int leftLen = currentLen - currentLen/length*length;
+        if(leftLen + stickLen>length ){
+            return false;
+        }
+        return true;
     }
 
+
+    public static void main(String[] args) {
+        int[]stick = new int[]{1,5,6,7,8};
+//        System.out.println(new Solution().makesquare(stick));
+//        double d = 1e-6;
+//        System.out.println(d );
+
+        System.out.println(Arrays.toString(stick));
+//        Arrays.asList();
+    }
 }
